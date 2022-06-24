1259eb6c4c375107ec50496c6cb231c20f6d642c
jxshidashuaibi <1271507813@qq.com>
2022-06-24 15:37:00 +0800

    hot_fix-idea-03

--- src/GIt.java
+++ src/GIt.java
@@ -1,7 +1,8 @@
 public class GIt {
     public static void main(String[] args){
-        int a =10;
-        System.out.println(a);
+        int a =15;
+        double b=10;
+        System.out.println(a+b);
         System.out.println("hello dashuaibi:jx");
         System.out.println("来自hot-fix分支的修改");
     }
